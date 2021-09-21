8efc5a6514c6474c5a64da16530f339045a3f432
fanda521 <1056015243@qq.com>
2021-09-21 13:51:25 +0800

    初始化

---
+++ src/main/java/com/wang/screwtoolstudy/ScrewToolStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wang.screwtoolstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewToolStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrewToolStudyApplication.class, args);
+    }
+
+}
